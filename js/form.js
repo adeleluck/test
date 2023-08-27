dc2e2fa98d249b3a0c7faac5831b113bb7a4008d
Adelya <adzgievaadele@gmail.com>
2023-08-28 00:08:20 +0300

    task 2.5

---
+++ js/form.js
@@ -0,0 +1,16 @@
+//Задание 2.5 Форма
+
+function handleFormSubmit(event) {
+  event.preventDefault();
+  
+  const data = new FormData(event.target);
+  
+  const value  = Object.fromEntries(data.entries());
+  
+  const results = document.querySelector('.results pre');
+  results.innerText = JSON.stringify(value , null, 2);
+}
+
+const form = document.querySelector('.my-form');
+form.addEventListener('submit', handleFormSubmit);
+
--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
 //Задание 2.1 Скрыть/Показать блок с заголовком
 
 var blockHideShow = true;
-buttOne = document.getElementById('butt_one');
+btnOne = document.getElementById('btn_one');
 blockOne = document.getElementById('block_one');
 
-buttOne.onclick = function(){
+btnOne.onclick = function(){
 	if (blockHideShow){
 			blockOne.classList.add("d-none");
 			blockHideShow = false;
@@ -18,12 +18,12 @@ buttOne.onclick = function(){
 
 var changePosition = true
 
-buttTwo = document.getElementById('butt_two');
+btnTwo = document.getElementById('btn_two');
 
 var itemList = document.querySelector('.itemList');
 var item = document.querySelectorAll('.item');
 
-buttTwo.onclick = function () {
+btnTwo.onclick = function () {
 	
 		if(changePosition){
 			itemList.insertBefore(item[2], item[1]);
@@ -49,49 +49,3 @@ const acceptMyModalDialogButton = document.getElementById("my-modal-dialog-accep
         myModalDialog.showModal();
     }
 	
-//Задание 2.4 Форма
-
-/*
-const form = document.querySelector('my-form');
-
-form.addEventListener('submit', (e) => {
-  e.preventDefault();  
-  
-  const selectOne = form.elements['FormCustomSelectOne'];
-  const selectTwo = form.elements['FormCustomSelectTwo'];
-  const selectThree = form.elements['FormCustomSelectThree'];
-  const selectFour = form.elements['FormCustomSelectFour'];
-  const selectFive = form.elements['FormCustomSelectFive'];
-  const inputOne = form.elements['myInputOne'];
-  const inputTwo = form.elements['myInputTwo'];
-  
-  
-  valid = true;
-  if (inputOne.value == ""){
-		alert ( "Пожалуйста заполните поле 'Поле для ввода № 1'." );
-		valid = false;
-  }
-   if (inputTwo.value == ""){
-		alert ( "Пожалуйста заполните поле 'Поле для ввода № 2'." );
-		valid = false;
-  }
-	
-	return valid;	
-	
-});
-*/
-
-function handleFormSubmit(event) {
-  event.preventDefault();
-  
-  const data = new FormData(event.target);
-  
-  const value  = Object.fromEntries(data.entries());
-  
-  const results = document.querySelector('.results pre');
-  results.innerText = JSON.stringify(value , null, 2);
-}
-
-const form = document.querySelector('.my-form');
-form.addEventListener('submit', handleFormSubmit);
-
